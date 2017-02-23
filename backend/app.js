7a3ac7d84d3ae9e6c60dd195077fd6d663408666
Oskar Jönefors <oskar.jonefors@gmail.com>
2017-02-23 22:01:45 +0100

    Removed unused views.

--- backend/app.js
+++ backend/app.js
@@ -25,12 +25,6 @@ if (process.argv.some(function(arg) {
     mongoose.connect(Cfg.DB_ADDRESS);
 }
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
