34a73be99fd031856e303180b14d2560e3332ce1
Jesper Olsson <jespols@student.chalmers.se>
2017-03-11 11:45:49 +0100

    Backend now allows all origins, not only localhost

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ if (process.argv.some(function(arg) {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
